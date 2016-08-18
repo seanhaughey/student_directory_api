b5df38a3871da29ccd76e83a11095e17a91d98e9
Sean Haughey <sdhaughey@gmail.com>
2016-08-18 15:04:24 -0500

    created student route file

---
+++ routes/students.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const Student = require('../models/student');
+
+router.get('/', function(req, res, next) {
+
+});
+
+module.exports = router;

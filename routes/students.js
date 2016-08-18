917da341727dce70fbe9c0a2c2d4ad1dcf60dd51
Sean Haughey <sdhaughey@gmail.com>
2016-08-18 15:15:17 -0500

    added students GET route

--- routes/students.js
+++ routes/students.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const Student = require('../models/student');
+var Student = require('../models/student');
 
 router.get('/', function(req, res, next) {
-
+  Student.find({}, function (err, students) {
+    if (err) {
+      res.status(500).send();
+    } else {
+      res.json(students);
+    }
+  })
 });
 
 module.exports = router;
